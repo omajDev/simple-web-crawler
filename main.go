9f990cbab7bad9ef771f2fe81670f45cdecbea04
Omar Mohammed Salim Al Junaibi <omajdev@gmail.com>
2020-10-20 03:17:49 +0400

    refactor

--- main.go
+++ main.go
@@ -3,110 +3,49 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"golang.org/x/net/html"
 )
 
-// Helper function to pull the href attribute from a Token
-func getHref(t html.Token) (ok bool, href string) {
-	// Iterate over token attributes until we find an "href"
-	for _, a := range t.Attr {
-		if a.Key == "href" {
-			href = a.Val
-			ok = true
+func f(n *html.Node) {
+
+	for c := n; c != nil; c = c.NextSibling {
+		if c.FirstChild != nil {
+			f(c.FirstChild)
+		}
+
+		if c.Data == "a" {
+			for _, a := range c.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+				}
+			}
 		}
 	}
-
-	// "bare" return will return the variables (ok, href) as
-	// defined in the function definition
-	return
 }
+func crawl(link string) {
 
-// Extract all http** links from a given webpage
-func crawl(url string, ch chan string, chFinished chan bool) {
-	resp, err := http.Get(url)
-
-	defer func() {
-		// Notify that we're done after this function
-		chFinished <- true
-	}()
-
+	resp, err := http.Get(link)
 	if err != nil {
-		fmt.Println("ERROR: Failed to crawl:", url)
+		fmt.Println("ERROR: Failed to crawl:", link)
 		return
 	}
-
-	b := resp.Body
-	defer b.Close() // close Body when the function completes
-
-	z := html.NewTokenizer(b)
-
-	for {
-		tt := z.Next()
-
-		switch {
-		case tt == html.ErrorToken:
-			// End of the document, we're done
-			return
-		case tt == html.StartTagToken:
-			t := z.Token()
-
-			// Check if the token is an <a> tag
-			isAnchor := t.Data == "a"
-			if !isAnchor {
-				continue
-			}
-
-			// Extract the href value, if there is one
-			ok, url := getHref(t)
-			if !ok {
-				continue
-			}
-
-			// Make sure the url begines in http**
-			hasProto := strings.Index(url, "http") == 0
-			if hasProto {
-				ch <- url
-			}
-		}
+	defer resp.Body.Close()
+	n, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	f(n)
 }
 
 func main() {
-	foundUrls := make(map[string]bool)
 	seedUrls := []string{
-		"https://markmcgranaghan.com",
-		"https://github.com/mmcgrana/gobyexample",
-		"https://github.com/mmcgrana/gobyexample#license",
-		"http://golang.org",
+		"https://en.wikipedia.org/wiki",
 	}
-
-	// Channels
-	chUrls := make(chan string)
-	chFinished := make(chan bool)
-
-	// Kick off the crawl process (concurrently)
+	ch := make(chan string)
+	defer close(ch)
 	for _, url := range seedUrls {
-		go crawl(url, chUrls, chFinished)
+		crawl(url)
 	}
-
-	// Subscribe to both channels
-	for c := 0; c < len(seedUrls); {
-		select {
-		case url := <-chUrls:
-			foundUrls[url] = true
-		case <-chFinished:
-			c++
-		}
-	}
-
-	// We're done! Print the results...
-	fmt.Printf("\nFound %d unique urls:\n", len(foundUrls))
-
-	for url := range foundUrls {
-		fmt.Println(" - " + url)
-	}
-
-	close(chUrls)
 }
