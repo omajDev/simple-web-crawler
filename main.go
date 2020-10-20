1dbdf4e7fe2a9fe9975734e682331299c9e2fd5d
Omar Mohammed Salim Al Junaibi <omajdev@gmail.com>
2020-10-20 22:52:03 +0400

    refactor

--- main.go
+++ main.go
@@ -1,51 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
 	"golang.org/x/net/html"
 )
 
-func f(n *html.Node) {
-
-	for c := n; c != nil; c = c.NextSibling {
-		if c.FirstChild != nil {
-			f(c.FirstChild)
-		}
-
-		if c.Data == "a" {
-			for _, a := range c.Attr {
-				if a.Key == "href" {
-					fmt.Println(a.Val)
-				}
-			}
-		}
-	}
-}
-func crawl(link string) {
-
-	resp, err := http.Get(link)
+func crawl(link chan string, client http.Client, f func(n *html.Node)) {
+	req, err := http.NewRequest(http.MethodGet, <-link, nil)
 	if err != nil {
-		fmt.Println("ERROR: Failed to crawl:", link)
+		log.Println("ERROR: Failed to crawl:", err)
 		return
 	}
-	defer resp.Body.Close()
-	n, err := html.Parse(resp.Body)
+	req.Header.Set("Accept", "text/html")
+	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("ERROR: Failed to crawl:", link)
+		return
+	}
+	defer res.Body.Close()
+	n, err := html.Parse(res.Body)
+	if err != nil {
+		log.Println(err)
 		return
 	}
 	f(n)
 }
 
 func main() {
+	const maxWorker = 5
 	seedUrls := []string{
 		"https://en.wikipedia.org/wiki",
+		"https://blog.hubspot.com/",
 	}
+	var client http.Client
 	ch := make(chan string)
 	defer close(ch)
-	for _, url := range seedUrls {
-		crawl(url)
+	var f func(n *html.Node)
+	f = func(n *html.Node) {
+
+		for c := n; c != nil; c = c.NextSibling {
+			if c.FirstChild != nil {
+				f(c.FirstChild)
+			}
+
+			if c.Data == "a" {
+				for _, a := range c.Attr {
+					if a.Key == "href" {
+						log.Println(a.Val)
+					}
+				}
+			}
+		}
 	}
+	for i := 0; i < maxWorker; i++ {
+		go crawl(ch, client, f)
+	}
+	for _, url := range seedUrls {
+		ch <- url
+	}
+	select {}
 }
