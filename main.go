a830fd1154b7b9c1521f4cc1a0a774594b915a99
Omar Mohammed Salim Al Junaibi <omajdev@gmail.com>
2020-10-22 16:13:53 +0400

    refactor

--- main.go
+++ main.go
@@ -7,19 +7,20 @@ import (
 	"golang.org/x/net/html"
 )
 
-func crawl(link chan string, client http.Client, f func(n *html.Node)) {
-	req, err := http.NewRequest(http.MethodGet, <-link, nil)
+func crawl(link string, client *http.Client, f func(n *html.Node)) {
+
+	req, err := http.NewRequest(http.MethodGet, link, nil)
 	if err != nil {
-		log.Println("ERROR: Failed to crawl:", err)
 		return
 	}
 	req.Header.Set("Accept", "text/html")
+
 	res, err := client.Do(req)
 	if err != nil {
-		log.Println("ERROR: Failed to crawl:", link)
 		return
 	}
 	defer res.Body.Close()
+
 	n, err := html.Parse(res.Body)
 	if err != nil {
 		log.Println(err)
@@ -30,35 +31,44 @@ func crawl(link chan string, client http.Client, f func(n *html.Node)) {
 
 func main() {
 	const maxWorker = 5
+	isVisted := make(map[string]struct{})
 	seedUrls := []string{
 		"https://en.wikipedia.org/wiki",
 		"https://blog.hubspot.com/",
 	}
 	var client http.Client
-	ch := make(chan string)
+	ch := make(chan string, maxWorker)
 	defer close(ch)
 	var f func(n *html.Node)
 	f = func(n *html.Node) {
-
 		for c := n; c != nil; c = c.NextSibling {
 			if c.FirstChild != nil {
 				f(c.FirstChild)
 			}
-
-			if c.Data == "a" {
+			switch c.Data {
+			case "a":
 				for _, a := range c.Attr {
 					if a.Key == "href" {
-						log.Println(a.Val)
+						ch <- a.Val
 					}
 				}
 			}
 		}
 	}
-	for i := 0; i < maxWorker; i++ {
-		go crawl(ch, client, f)
-	}
+
 	for _, url := range seedUrls {
-		ch <- url
+		go crawl(url, &client, f)
+		isVisted[url] = struct{}{}
 	}
-	select {}
+	for {
+		select {
+		case url := <-ch:
+			if _, ok := isVisted[url]; !ok {
+				go crawl(url, &client, f)
+				log.Println(url)
+				isVisted[url] = struct{}{}
+			}
+		}
+	}
+
 }
